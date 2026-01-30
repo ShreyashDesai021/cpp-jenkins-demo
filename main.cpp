707a9d9143df4cc40a7d5ffecd7ac046ac63cb62
ShreyashDesai021 <dshreyash04@gmail.com>
2026-01-30 12:38:36 +0530

    Implement command-line calculator in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int main() {
-    cout << "Hello from C++ executed by Jenkins!" << endl;
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        cout << "Usage: calculator <num1> <operator> <num2>" << endl;
+        return 1;
+    }
+
+    int a = atoi(argv[1]);
+    char op = argv[2][0];
+    int b = atoi(argv[3]);
+
+    switch (op) {
+        case '+': cout << "Result: " << a + b; break;
+        case '-': cout << "Result: " << a - b; break;
+        case '*': cout << "Result: " << a * b; break;
+        case '/': 
+            if (b != 0) cout << "Result: " << a / b;
+            else cout << "Division by zero error";
+            break;
+        default:
+            cout << "Invalid operator";
+    }
     return 0;
 }
